6bcce237bf9f5ff384a83a9e90fe805815d60dfc
Syed Baqir Ali <gitbaq@gmail.com>
2023-04-08 22:12:28 +1000

    added another endpoint

--- src/main/java/com/sba/blogservice/controller/BlogController.java
+++ src/main/java/com/sba/blogservice/controller/BlogController.java
@@ -3,6 +3,7 @@ package com.sba.blogservice.controller;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,10 @@ public class BlogController {
     @GetMapping
     public String hello(){
         return "Alhamdolillah Rabbil Alameen";
+    }
+
+    @GetMapping("/msg")
+    public String message(@RequestParam String msg){
+        return "Message received: " + msg;
     }
 }
