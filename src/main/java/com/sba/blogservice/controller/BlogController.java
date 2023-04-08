0bd20d728a189092f21813c856ba13e830929f34
Syed Baqir Ali <gitbaq@gmail.com>
2023-04-08 20:08:50 +1000

    build test

--- src/main/java/com/sba/blogservice/controller/BlogController.java
+++ src/main/java/com/sba/blogservice/controller/BlogController.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class BlogController {
     @GetMapping
     public String hello(){
-        return "Bismillah";
+        return "Alhamdolillah";
     }
 }
